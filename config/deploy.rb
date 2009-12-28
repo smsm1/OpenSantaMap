62e95a89d758debfae2a7ff27d5b9e30fbf4b9c4
Shaun McDonald <shaun@shaunmcdonald.me.uk>
2009-12-28 02:35:52 +0000

    Deploy tweaks

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,14 +10,21 @@ role :web, "opensantamap.org"                          # Your HTTP server, Apach
 role :app, "opensantamap.org"                          # This may be the same as your `Web` server
 role :db,  "opensantamap.org", :primary => true # This is where Rails migrations will run
 
+set :use_sudo, false
 
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
 # these http://github.com/rails/irs_process_scripts
 
 namespace :deploy do
-  task :start {}
-  task :stop {}
+  task :start, :roles => :app do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+
+  task :stop do
+    # do nothing
+  end
+
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
